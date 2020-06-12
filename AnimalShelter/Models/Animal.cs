03fb2e0d120af883f13fdc8803479b2fd740c0c3
Tyler Bates <Fondlytyler@gmail.com>
2020-06-12 09:07:59 -0700

    add animal class in model folder.

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+    [Required]
+    public string Species { get; set; }
+    [Required]
+    [StringLength(500, ErrorMessage = "Description must be less than 500 characters")]
+    public string Description { get; set; }
+    [Required]
+    public int Age { get; set; }
+    public string ImageUrl { get; set; }
+    
+  }
+}
