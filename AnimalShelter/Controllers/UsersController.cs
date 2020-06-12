fd3968612fff4747dda1586e245757b0a3fb00f2
Tyler Bates <Fondlytyler@gmail.com>
2020-06-12 09:35:40 -0700

    add authentication route to user controller.

--- AnimalShelter/Controllers/UsersController.cs
+++ AnimalShelter/Controllers/UsersController.cs
@@ -8,26 +8,41 @@ using System;
 namespace AnimalShelter.Controllers
 {
   [Authorize]
-    [ApiController]
-    [Produces("application/json")]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
-    {
-        private AnimalShelterContext _db;
-        
-        private IUserService _userService;
+  [ApiController]
+  [Produces("application/json")]
+  [Route("api/[controller]")]
+  public class UsersController : ControllerBase
+  {
+      private AnimalShelterContext _db;
+      
+      private IUserService _userService;
 
-        public UsersController(AnimalShelterContext db, IUserService userService)
-        {
-            _db = db;
-            _userService = userService;
-        }
+      public UsersController(AnimalShelterContext db, IUserService userService)
+      {
+          _db = db;
+          _userService = userService;
+      }
 
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var users =  _userService.GetAll();
-            return Ok(users);
-        }
+      [AllowAnonymous]
+      [HttpPost("authenticate")]
+      public IActionResult Authenticate([FromBody]User userParam)
+      {
+          // Console.WriteLine($"\n\n\n\n\n" + "We are inside Users Controller anonymous allowed {userParam.Username} and {userParam.Password}" + "\n\n\n\n\n");
+          var user = _userService.Authenticate(userParam.Username, userParam.Password);
+          
+          // Console.WriteLine($"\n\n\n\n\n" + "value for the user variable is = {user}" + "\n\n\n\n\n");
+          if (user == null)
+              return BadRequest(new { message = "Definitely not the correct username or password" });
+
+          return Ok(user);
+      }
+
+      [AllowAnonymous]
+      [HttpGet]
+      public IActionResult GetAll()
+      {
+          var users =  _userService.GetAll();
+          return Ok(users);
+      }
     }
 }
